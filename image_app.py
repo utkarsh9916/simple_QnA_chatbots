d34e399203d53d7e30f5fc1d3356f6c4773dfebc
utkarsh9916 <149650760+utkarsh9916@users.noreply.github.com>
2024-08-10 12:27:22 +0530

    QnA with image.py

--- image_app.py
+++ image_app.py
@@ -1,46 +1,3 @@
-# from dotenv import load_dotenv
-# load_dotenv()  ## loading all the enviroment variables
-# from PIL import Image
-
-# import streamlit as st
-# import os   
-# import google.generativeai as genai
-
-
-# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-# #  functions to load gemini pro model and get responses
-
-# model=genai.GenerativeModel("gemini-vision-pro")
-# def get_gemini_response(input,image):
-#     if input!="":
-#         response=model.generate_content([input,image])
-#     else:
-#         response=model.generate_content(image)
-#     return response.text
-
-
-# # initialize our streamlit app
-
-# st.set_page_config(page_title="Sawal-Jawab 2.0")
-# st.header("Gemini LLM Application")
-
-# input=st.text_input("Input:", key="Input")
-
-# uploaded_file= st.file_uploader("Choose_an_image.......",type=["jpg","jpeg","png"])
-# image = None
-# if uploaded_file is not None:
-#         image = Image.open(uploaded_file)
-#         st.image(image,caption="Uploaded Image", use_column_width=True)
-
-# submit= st.button("Tell me about the image ")
-
-
-# # when submit is clicked
-# if submit:
-#     response=get_gemini_response(input,image)
-#     st.subheader("The Response is ")
-#     st.write(response)
 
 
 from dotenv import load_dotenv
@@ -51,7 +8,8 @@ import streamlit as st
 import os   
 import google.generativeai as genai
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+genai.configure(api_key=os.getenv("Add your api key"))
 
 # Function to load Gemini Pro model and get responses
 model = genai.GenerativeModel("gemini-1.5-flash")
